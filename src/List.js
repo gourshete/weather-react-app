2a97b537c49b5d84e929aba965636faee3f9c8a1
gourshete <swapnilggourshete@gmail.com>
2020-09-20 23:13:28 +0530

    - add styling to items inside List component - display more info on List item

--- src/List.js
+++ src/List.js
@@ -1,4 +1,5 @@
 import React, { setState, Component } from 'react';
+import './List.css'
 
 export default class List extends Component{
 
@@ -12,15 +13,27 @@ export default class List extends Component{
       if(items.length>0){
       list = items.map((item) =>
         <div className='item'>
-            <label> {item.name} - {item.main.temp}</label>
+            <div className='header'>
+              <label className='temp'> {item.main.temp}</label>
+            </div>
+            <div className='city-name'>
+              <strong>{item.name}</strong>
+            </div>
+            <div className='humidity'>
+              <label> Humidity - {item.main.humidity}</label>
+            </div>
+            <div className='weather'>
+              <label>
+                weather - {item.weather[0].description}
+              </label>
+            </div>
         </div>
         );
       }
     if(items.length>0){
         return (
             <div className="list">
-                List
-                <div className="temperature">
+                <div className="container">
                 { list }
                 </div>      
             </div>
